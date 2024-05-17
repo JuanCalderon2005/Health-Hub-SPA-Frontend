f6749315e292290127b476a25c7a20b2d1a5180d
juan <juanestebancredifacil@gmail.com>
2024-05-17 16:01:01 -0500

    update files

--- app/scenes/private/home/home.js
+++ app/scenes/private/home/home.js
@@ -5,8 +5,8 @@ export function HomeScene() {
   const pageContent = `
     <p>Welcome to the home view.</p>
     <section class="${styles['grid-container']}">
-    <div class="${styles['left-container']}">1</div>
       <div class="${styles['left-container']}">2</div>
+      <div class="${styles['other-container']}">1</div>
       <div class="${styles['right-container']}">3</div>
     </section>
   `;
