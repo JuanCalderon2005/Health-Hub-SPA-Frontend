ffd665b2382bd0621fa90acc8e1afff35a88da3f
juan <juanestebancredifacil@gmail.com>
2024-05-20 14:33:14 -0500

    update files

--- app/helpers/routes.js
+++ app/helpers/routes.js
@@ -1,14 +1,10 @@
 import { LoginPage } from '../scenes/public/login';
-import { HomeScene } from '../scenes/private/home';
-import { ReportScene } from '../scenes/private/reports';
-import { SettingsScene } from '../scenes/private/settings';
+import { HomeScene } from '../scenes/private/Physician/home';
 
 
 export const routes = {
     private: [
-        { path: '/dashboard/home', component: HomeScene },
-        { path: '/dashboard/reports', component: ReportScene },
-        { path: '/dashboard/settings', component: SettingsScene }
+        { path: '/dashboard/home', component: HomeScene }
     ],
     public: [
         { path: '/login', component: LoginPage }
